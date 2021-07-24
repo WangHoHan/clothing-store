8e828c3ca22d78669e039cf25b104ff1eb3515f2
Jakub Szczapa <jakuubs11@gmail.com>
2021-07-24 23:53:18 +0200

    initialize backend

---
+++ backend/src/main/java/com/clothingstore/ClothingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.clothingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingStoreApplication.class, args);
+    }
+
+}
