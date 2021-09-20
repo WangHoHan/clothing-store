ace55b797a238be9b93e3c8185e2ea0c2ddd2a91
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-09-20 19:28:32 +0200

    nav time added

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -8,9 +8,21 @@ import { ShoppingBagService } from "../../services/shopping-bag/shopping-bag.ser
 })
 export class NavigationComponent implements OnInit {
 
+  public hour! : string;
+  public minute! : string;
+  public amPm! : string;
+
   constructor(private shoppingBagService : ShoppingBagService) { }
 
   ngOnInit() : void {
+    setInterval(() => {
+      let date : Date = new Date(Date.now());
+      let hourAmPm : string = new Intl.DateTimeFormat('en', { hour: '2-digit' }).format(date);
+      this.hour = hourAmPm.split(" ")[0];
+      this.hour = hourAmPm.split(" ")[0];
+      this.minute = new Intl.DateTimeFormat('en', { minute: '2-digit' }).format(date);
+      this.amPm = hourAmPm.split(" ")[1];
+    }, 1000);
     this.updateMousePosition();
   }
 
